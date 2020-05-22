092536026bc771f9504e14227e4c105b8ec35241
Hans Marius Gunleiksrud <hansag91@gmail.com>
2020-05-22 16:05:05 +0200

    funker delvis

--- sel_test.py
+++ sel_test.py
@@ -3,38 +3,38 @@ import time
 from collections import defaultdict
 
 
-browser = webdriver.Chrome("/mnt/d/python_scripts/chromedriver.exe")
+browser = webdriver.Chrome("/Users/Hansi/Desktop/pyScripts/chromedriver")
 
 
 #badetassen
 plasser = defaultdict(int)
 browser.get('https://badetassen.no')
 
-i = 0
-e = 0
+i = 1
+e = 1
 
 
 while 1:
     time.sleep(2)
     try:
-        region = browser.find_elements_by_css_selector('ul')[i]
-        region.click
-    except IndexError as error:
+        region = browser.find_element_by_css_selector("div > div > div > div.region-filter > ul > li:nth-child(%d) > a"%i).click()
+        #bør bruke wait()
+        i+=1
+
+        while 1:
+
+            time.sleep(1)
+            try:#må fikse selector
+                plass = browser.find_elements_by_class_name("div > div > div > div.area-list.Grid.Grid-center.Grid--cols-3 > div:nth-child(%d) > a"%e).click()
+                
+                browser.execute_script("window.history.go(-1)")
+                
+                e+=1
+            except Exception as error:
+                break
+
+    
+       
+    except Exception as error:
         print(error)
         break
-    
-    while 1:
-
-        time.sleep(1)
-        try:
-            plass = browser.find_elements_by_class_name('area-list-item')[e]
-            plass.click()
-            time.sleep(1)
-            browser.execute_script("window.history.go(-1)")
-            
-            e+=1
-        except IndexError as error:
-            break
-
-    i+=1
-    time.sleep(1)
